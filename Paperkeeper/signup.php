cfd51a025d52b1309112fd04841e46887300b63d
leftttttt <3364361510@qq.com>
2026-08-14 14:34:32 +1200

    Add files via upload
    php for sign up page

--- Paperkeeper/signup.php
+++ Paperkeeper/signup.php
@@ -1,26 +1,54 @@
 <?php
 require_once "config.php";
 
-$email = $username = $password = $repassword = "";
+$email = $username = $password = $repassword = $param_email = $param_password = "";
 $email_err = $username_err = $password_err = $repassword_err = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+if (empty(trim($_POST["email"]))){
+    $email_err = "Please enter an email.";
+} elseif(!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)){
+    $email_err = "Please enter a valid email address.";
+} else{
+    $sql = "SELECT id FROM user WHERE email = ?";
+
+    if($stmt = mysqli_prepare($link, $sql)){
+
+    mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+    $param_email = trim($_POST["email"]);
+
+        if(mysqli_stmt_execute($stmt)){
+
+            mysqli_stmt_store_result($stmt);
+
+            if(mysqli_stmt_num_rows($stmt) == 1){
+                $email_err = "This email is already taken.";
+            } else{
+                $email = trim($_POST["email"]);
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+}
 
 if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
         $username_err = "Username can only contain letters, numbers, and underscores.";
     } else{
-        
-        $sql = "SELECT id FROM users WHERE username = ?";
+        $sql = "SELECT id FROM user WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            
+
             $param_username = trim($_POST["username"]);
-            
             
             if(mysqli_stmt_execute($stmt)){
                 
@@ -35,7 +63,6 @@ if(empty(trim($_POST["username"]))){
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            
             mysqli_stmt_close($stmt);
         }
     }
@@ -60,16 +87,17 @@ if(empty(trim($_POST["username"]))){
     }
     
     
-    if(empty($username_err) && empty($password_err) && empty($repassword_err)){
+    if(empty($email_err) && empty($username_err) && empty($password_err) && empty($repassword_err)){
         
         
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO user (email, username, password) VALUES (?, ?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sss", $param_email, $param_username, $param_password);
             
             
+            $param_email = $email;
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT);
             
@@ -120,25 +148,33 @@ if(empty(trim($_POST["username"]))){
     </div>
     </div>
 
+    <form method="POST" action="signup.php">
+
+
+
     <div class="info">
     <div class="email">
         <label for="email" style="color: #274c77;">Email:</label>
-        <input type="email" class="input" placeholder="Enter email" id="email" name="email">
+        <input type="email" class="input" placeholder="Enter email" id="email" name="email" value="<?php echo $email; ?>">
+        <span style="display: block; margin-top: 10px; color: #dc3545;"><?php echo $email_err; ?></span>
     </div>
 <br><br>
     <div class="username">
         <label for="username" style="color: #274c77;">Username:</label>
-        <input type="text" class="input" placeholder="Enter username" id="username" name="username">
+        <input type="text" class="input" placeholder="Enter username" id="username" name="username" value="<?php echo $username; ?>">
+        <span style="display: block; margin-top: 10px; color: #dc3545;"><?php echo $username_err; ?></span>
     </div>
 <br><br>
     <div class="password">
         <label for="password" style="color: #274c77;">Password:</label>
-        <input type="password" class="input" placeholder="Enter password" id="password" name="password">
+        <input type="password" class="input" placeholder="Enter password" id="password" name="password" value="<?php echo $password; ?>">
+        <span style="display: block; margin-top: 10px; color: #dc3545;"><?php echo $password_err; ?></span>
     </div>
 <br><br>
     <div class="repassword">
         <label for="repassword" style="color: #274c77;">Repeat Password:</label>
-        <input type="password" class="input" placeholder="Repeat password" id="repassword" name="repassword">
+        <input type="password" class="input" placeholder="Repeat password" id="repassword" name="repassword" value="<?php echo $repassword; ?>">
+        <span style="display: block; margin-top: 10px; color: #dc3545;"><?php echo $repassword_err; ?></span>
     </div>
 <br><br>
 
@@ -157,5 +193,6 @@ if(empty(trim($_POST["username"]))){
             </button>
 
     </div>
+    </form>
 </body>
 </html>
